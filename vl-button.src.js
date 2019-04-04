2833be843d70187a8174f1e8887dd8f4dbba5d62
Tom Coemans <tom.coemans@gmail.com>
2019-04-04 15:35:51 +0200

    WEBC-44 native vl element

--- vl-button.src.js
+++ vl-button.src.js
@@ -1,25 +1,4 @@
-import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
-
-(() => {
-    const id = 'vl-button-style';
-    addStyle();
-
-    function addStyle() {
-        if (!document.head.querySelector('#' + id)) {
-            var style = getStyle();
-            document.head.appendChild(style);
-        }
-    }
-
-    function getStyle() {
-        var link = document.createElement('link');
-        link.setAttribute('id', id);
-        link.setAttribute('rel', 'stylesheet');
-        link.setAttribute('type', 'text/css');
-        link.setAttribute('href', '../style.css');
-        return link;
-    }
-})();
+import { NativeVlElement } from '/node_modules/vl-ui-core/vl-core.js';
 
 /**
  * `vl-button``
@@ -27,7 +6,7 @@ import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
  * 
  * @demo demo/vl-button.html
  */
-export class VlButton extends VlElement(HTMLButtonElement) {
+export class VlButton extends NativeVlElement(HTMLButtonElement) {
     static get _observedAttributes() {
         return [];
     }
