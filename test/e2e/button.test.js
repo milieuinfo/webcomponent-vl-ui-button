9a5655c71d14a2a0bf4bd708b8ef5fa4ce82278e
Tom Coemans <tom.coemans@gmail.com>
2019-10-31 16:42:10 +0100

    chai as promised

--- test/e2e/button.test.js
+++ test/e2e/button.test.js
@@ -2,22 +2,20 @@
 const ButtonPage = require('../e2e/pages/button.page')
 const { Builder } = require('selenium-webdriver');
 const chai = require('chai');
-const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var assert = chai.assert;
 
 describe('Button', function () {
     const driver = new Builder().forBrowser('chrome').build();
 
     it('Als ik op de knop klik, word mijn actie geregistreerd.', async () => {
         await driver.get('http://localhost:8080/demo/vl-button.html');
-
         let buttonPage = new ButtonPage(driver);
         const primaryButton = await buttonPage.primaryButton();
-        let text = await primaryButton.getText();
-        expect(text).to.equal('Gegevens opslaan');
+        assert.eventually.equal(primaryButton.getText(), 'Gegevens opslaan');
         await primaryButton.click();
-        text = await primaryButton.getText();
-        expect(text).to.equal('Klik geregistreerd');
-        return Promise.resolve();
+        assert.eventually.equal(primaryButton.getText(), 'Klik geregistreerd');
     });
 
     after(() => driver && driver.quit());
