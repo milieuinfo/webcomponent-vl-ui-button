cc6c882e7ae03cf0ad04cc80dbf861f8e0e109f1
cambiph <incubusattax@gmail.com>
2019-11-05 15:50:08 +0100

    Verwijder webdriver-manager in favor of node-chromedriver en geckodriver

--- test/e2e/test.js
+++ test/e2e/test.js
@@ -6,7 +6,8 @@ const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var assert = chai.assert;
 
-process.env['webdriver.opera.driver'] = "./operadriver";
+process.env['webdriver.gecko.driver'] = "../../node_modules/geckodriver/geckodriver";
+process.env['webdriver.chrome.driver'] = "../../node_modules/chromedriver/lib/chromedriver"
 
 let driver;
 
