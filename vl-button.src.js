ab5fa95971b618c05da6e18e86e15e2e99ce7b34
Tom Coemans <tom.coemans@gmail.com>
2020-01-29 09:12:19 +0100

    fix

--- vl-button.src.js
+++ vl-button.src.js
@@ -1,7 +1,49 @@
 import { NativeVlElement, define } from 'vl-ui-core';
-import { VlLinkElement } from "vl-ui-link";
 import { VlPillElement } from "vl-ui-pill";
 import { VlInputAddonElement } from "vl-ui-input-addon";
+
+export const VlButtonElement = (SuperClass) => {
+    return class extends NativeVlElement(SuperClass) {
+        static get _observedAttributes() {
+            return [];
+        }
+
+        static get _observedClassAttributes() {
+            return ['disabled', 'error', 'block', 'large', 'wide', 'narrow', 'secondary', 'tertiary', 'loading'];
+        }
+
+        connectedCallback() {
+            this.classList.add('vl-button');
+            setTimeout(() => {
+                this._setIconClass();
+            });
+        }
+
+        get _classPrefix() {
+            return 'vl-button--';
+        }
+
+        get _stylePath() {
+            return '../style.css';
+        }
+
+        _setIconClass() {
+            const icon = this.querySelector('[is="vl-icon"]');
+            if (icon) {
+                let suffix = '';
+                suffix += icon.hasAttribute('before') ? '-before' : '';
+                suffix += icon.hasAttribute('after') ? '-after' : '';
+                this.classList.add(this._classPrefix + 'icon' + suffix);
+                icon.classList.add('vl-button__icon');
+                if (suffix) {
+                    icon.classList.add('vl-button__icon-' + suffix);
+                }
+                icon.classList.remove('vl-icon--before');
+                icon.classList.remove('vl-icon--after');
+            }
+        }
+    }
+};
 
 /**
  * VlButton
@@ -24,50 +66,30 @@ import { VlInputAddonElement } from "vl-ui-input-addon";
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-button/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-button.html|Demo}
  */
-export class VlButton extends NativeVlElement(HTMLButtonElement) {
-    static get _observedAttributes() {
-        return [];
-    }
+export class VlButton extends VlButtonElement(HTMLButtonElement) {}
 
-    static get _observedClassAttributes() {
-        return ['disabled', 'error', 'block', 'large', 'wide', 'narrow', 'secondary', 'tertiary', 'loading'];
-    }
-
-    connectedCallback() {
-        this.classList.add('vl-button');
-        setTimeout(() => {
-            this._setIconClass();
-        });
-    }
-
-    get _classPrefix() {
-        return 'vl-button--';
-    }
-
-    _setIconClass() {
-        const icon = this.querySelector('[is="vl-icon"]');
-        if (icon) {
-            let suffix = '';
-            suffix += icon.hasAttribute('before') ? '-before' : '';
-            suffix += icon.hasAttribute('after') ? '-after' : '';
-            this.classList.add(this._classPrefix + 'icon' + suffix);
-            icon.classList.add('vl-button__icon');
-            if (suffix) {
-                icon.classList.add('vl-button__icon-' + suffix);
-            }
-            icon.classList.remove('vl-icon--before');
-            icon.classList.remove('vl-icon--after');
-        }
-    }
-}
-
-export class VlButtonLink extends VlLinkElement(HTMLButtonElement) {}
+/**
+ * VlLinkButton
+ * @class
+ * @classdesc Gebruik de vl-link-button om een ​​call-to-action toe te voegen.
+ *
+ * @extends NativeVlElement
+ *
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-link.html|Demo}
+ */
+export class VlLinkButton extends VlButtonElement(HTMLAnchorElement) {}
 
 export class VlButtonPill extends VlPillElement(NativeVlElement(HTMLButtonElement)) {
     constructor() {
         super();
         this.classList.add('vl-pill');
         this.classList.add('vl-pill--clickable');
+    }
+
+    get _stylePath() {
+        return '../style.css';
     }
 }
 
@@ -76,6 +98,6 @@ export class VlButtonInputAddon extends VlInputAddonElement(NativeVlElement(HTML
 } 
 
 define('vl-button', VlButton, {extends: 'button'});
-define('vl-button-link', VlButtonLink, {extends: 'button'});
 define('vl-button-pill', VlButtonPill, {extends: 'button'});
 define('vl-button-input-addon', VlButtonInputAddon, {extends: 'button'});
+define('vl-link-button', VlLinkButton, {extends: 'a'});
