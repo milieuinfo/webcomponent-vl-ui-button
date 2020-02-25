a4b11b49ddb08fb6ed357eb4eafded80a021543f
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-02-25 12:55:03 +0100

    UIG-529 commit space

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+module.exports = { 
+    VlButton: require('../dist/vl-button.src.js').VlButton,
+    VlButtonLink: require('../dist/vl-button.src.js').VlButtonLink,
+    VlButtonPill: require('../dist/vl-button.src.js').VlButtonPill,
+    VlButtonInputAddon: require('../dist/vl-button.src.js').VlButtonInputAddon
+}
