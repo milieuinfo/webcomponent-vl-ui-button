ef6879c6057856d84241baca41df975a83c6c6b8
cambiph <philippe.cambien@gmail.com>
2020-03-16 11:27:42 +0100

    Genereer 100 instanties van de demo

--- demo/performance.js
+++ demo/performance.js
@@ -10,10 +10,14 @@ JSDOM.fromFile("vl-button.html").then(dom => {
     dom.window.document.querySelectorAll('link[rel="stylesheet"]').forEach(s => stylesheets.push(s.outerHTML));
 
     const demo = dom.window.document.querySelectorAll('.demo')[0].outerHTML;
+    let demoMultiplied = '';
+    for (i = 0; i < 100; i++) {
+        demoMultiplied += demo;
+    }
 
-    const html = `<html><head>${scripts.join('')}${stylesheets.join('')}</head><body>${demo}</body></html>`;
+    const html = `<html><head>${scripts.join('')}${stylesheets.join('')}</head><body>${demoMultiplied}</body></html>`;
 
     fs.writeFileSync('performance.html', html, 'utf8', (err) => {
         if (err) throw err;
-    })
+    });
 });
