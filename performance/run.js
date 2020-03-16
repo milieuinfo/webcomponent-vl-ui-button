a8ab9401d3bc573bef247cf1b927691ed1aa4a0f
cambiph <philippe.cambien@vlaanderen.be>
2020-03-16 16:36:07 +0100

    Update performance/run.js
    Foutief tweemaal de chrome config gebruiken ipv 1 keer Chrome en 1 keer Firefox.
    
    Co-Authored-By: Pieter Beckers <28813026+Pieter-Beckers@users.noreply.github.com>

--- performance/run.js
+++ performance/run.js
@@ -5,7 +5,7 @@ if(process.platform === "darwin") {
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io:12.2.0 --config=config/config-firefox.json http://host.docker.internal:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
 } else if(process.platform === "linux") {
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io:12.2.0 --config=config/config-chrome.json --network=host http://host.docker.internal:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
-    execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io:12.2.0 --config=config/config-chrome.json --network=host http://host.docker.internal:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
+    execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io:12.2.0 --config=config/config-firefox.json --network=host http://host.docker.internal:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
 } else {
     console.error('This platform is not supported, exiting!');
     process.exit(1);
