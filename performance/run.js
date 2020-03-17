855bcbe0bf31ab5f5229be1402feeace0758d3e4
cambiph <philippe.cambien@gmail.com>
2020-03-17 13:08:42 +0100

    Geef mee of er vanop Bamboo gerund word

--- performance/run.js
+++ performance/run.js
@@ -1,16 +1,14 @@
 const { execSync } = require('child_process');
-const { os } = require('os');
-
-const regex = new RegExp(/bamboo/);
+const argv = require('yargs').argv;
 
 if(process.platform === "darwin") {
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-chrome.json http://host.docker.internal:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-firefox.json http://host.docker.internal:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
-} else if(process.platform === "linux" && !regex.test(os.hostname())) {
+} else if(process.platform === "linux" && !argv.bamboo) {
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-chrome.json --network=host http://localhost:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-firefox.json --network=host http://localhost:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
-} else if (regex.test(os.hostname())) {
-    console.log('Running on Bamboo ...')
+} else if (argv.bamboo) {
+    console.log('Running on Bamboo ...');
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-bamboo-chrome.json --network=host http://localhost:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-bamboo-firefox.json --network=host http://localhost:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
 } else {
