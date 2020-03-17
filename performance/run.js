0ee559addc7cee1580d2eaff8978e82ac5677d14
cambiph <philippe.cambien@gmail.com>
2020-03-17 13:02:52 +0100

    Add logging

--- performance/run.js
+++ performance/run.js
@@ -10,6 +10,7 @@ if(process.platform === "darwin") {
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-chrome.json --network=host http://localhost:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-firefox.json --network=host http://localhost:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
 } else if (regex.test(os.hostname())) {
+    console.log('Running on Bamboo ...')
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-bamboo-chrome.json --network=host http://localhost:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
     execSync('docker run --rm -v "$(pwd)":/sitespeed.io sitespeedio/sitespeed.io --config=config/config-bamboo-firefox.json --network=host http://localhost:8080/performance/performance.html', {stdio: 'inherit', cwd: __dirname});
 } else {
