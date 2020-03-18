3e162237341aba9b263564960a2f63e1254c4058
Sander Kleykens <sander.kleykens@avnu.be>
2020-03-18 08:54:48 +0100

    Met promises werken in preScript.js

--- performance/scripts/preScript.js
+++ performance/scripts/preScript.js
@@ -1,15 +1,21 @@
 const http = require('http');
 
-function poll() {
-    setTimeout(() => {
-        http.get('http://demo:8080', (res) => {
-            const { statusCode } = res;
-            if (statusCode !== 200) {
-                console.log('Not ready yet!');
-                poll();
-            }
-        });
-    }, 1000);
-}
+module.exports = async function(context, commands) {
+    return new Promise(resolve => {
+        function poll() {
+            setTimeout(() => {
+                http.get('http://demo:8080', (res) => {
+                    const { statusCode } = res;
+                    if (statusCode !== 200) {
+                        console.log('Not ready yet!');
+                        poll();
+                    } else {
+                        resolve();
+                    }
+                });
+            }, 1000);
+        }
 
-poll();
+        poll();
+    });
+}
