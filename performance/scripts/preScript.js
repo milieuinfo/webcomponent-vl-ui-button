b399521fb893d1b97320a7dcc9c65b6a08a45dfa
cambiph <philippe.cambien@gmail.com>
2020-03-18 09:08:08 +0100

    preScript refactor

--- performance/scripts/preScript.js
+++ performance/scripts/preScript.js
@@ -1,19 +1,26 @@
 const http = require('http');
 
-module.exports = async function(context, commands) {
+module.exports = async function (context) {
+
+    const hostname = 'demo';
+    const port = '8080';
+
     return new Promise(resolve => {
         function poll() {
             setTimeout(() => {
-                http.get('http://demo:8080', (res) => {
+                http.get(`http://${hostname}:${port}`, (res) => {
                     const { statusCode } = res;
-                    if (statusCode !== 200) {
-                        console.log('Not ready yet!');
-                        poll();
-                    } else {
+                    if (statusCode === 200) {
+                        context.log.info(`App available on ${hostname}:${port}.`);
                         resolve();
+                    } else {
+                        poll();
                     }
+                }).on('error', () => {
+                    context.log.error(`App not yet available on ${hostname}:${port}.`);
+                    poll();
                 });
-            }, 1000);
+            }, 5000);
         }
 
         poll();
