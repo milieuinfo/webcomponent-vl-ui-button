0d8ed18f9148d6ad7e073c9ef0c4735c20ad26e6
Sander Kleykens <sander.kleykens@avnu.be>
2020-03-18 08:43:11 +0100

    Probeer eens iets anders met het wait script

--- performance/scripts/preScript.js
+++ performance/scripts/preScript.js
@@ -1,13 +1,12 @@
 const http = require('http');
 
 function poll() {
-    setInterval(() => {
+    setTimeout(() => {
         http.get('http://demo:8080', (res) => {
             const { statusCode } = res;
             if (statusCode !== 200) {
                 console.log('Not ready yet!');
-            } else {
-                return;
+                poll();
             }
         });
     }, 1000);
