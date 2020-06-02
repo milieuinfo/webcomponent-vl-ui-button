0dbcb336f10e5b2d4ef2eced83f4cbb3700e09bc
cambiph <philippe.cambien@gmail.com>
2020-06-02 09:09:32 +0200

    Upgrade util en fix formatting adhv eslint

--- test/e2e/button.test.js
+++ test/e2e/button.test.js
@@ -1,121 +1,121 @@
 
-const { assert, driver } = require('vl-ui-core').Test.Setup;
-const { Config} = require('vl-ui-core').Test;
+const {assert, driver} = require('vl-ui-core').Test.Setup;
+const {Config} = require('vl-ui-core').Test;
 const VlButtonPage = require('./pages/vl-button.page');
 
 describe('vl-button', async () => {
-    const vlButtonPage = new VlButtonPage(driver);
+  const vlButtonPage = new VlButtonPage(driver);
 
-    before(async () => {
-        return vlButtonPage.load();
-    });
+  before(async () => {
+    return vlButtonPage.load();
+  });
 
-    it('als gebruiker wil ik dat mijn klik geregistreerd wordt wanneer ik op een knop klik', async () => {
-        const button = await vlButtonPage.getPrimaryButton();
-        await assert.eventually.equal(button.getText(), 'Gegevens opslaan');
-        await button.click();
-        await assert.eventually.equal(button.getText(), 'Klik geregistreerd');
-    });
+  it('als gebruiker wil ik dat mijn klik geregistreerd wordt wanneer ik op een knop klik', async () => {
+    const button = await vlButtonPage.getPrimaryButton();
+    await assert.eventually.equal(button.getText(), 'Gegevens opslaan');
+    await button.click();
+    await assert.eventually.equal(button.getText(), 'Klik geregistreerd');
+  });
 
-    it('als gebruiker wil ik niet dat mijn klik geregistreerd wordt wanneer ik op een disabled knop klik', async () => {
-        const button = await vlButtonPage.getDisabledButton();
-        await assert.eventually.equal(button.getText(), 'Gegevens opslaan');
-        await button.click();
-        await assert.eventually.equal(button.getText(), 'Gegevens opslaan');
-        await assert.eventually.isFalse(button.isEnabled());
-        await assert.eventually.isTrue(button.isDisabled());
-    });
+  it('als gebruiker wil ik niet dat mijn klik geregistreerd wordt wanneer ik op een disabled knop klik', async () => {
+    const button = await vlButtonPage.getDisabledButton();
+    await assert.eventually.equal(button.getText(), 'Gegevens opslaan');
+    await button.click();
+    await assert.eventually.equal(button.getText(), 'Gegevens opslaan');
+    await assert.eventually.isFalse(button.isEnabled());
+    await assert.eventually.isTrue(button.isDisabled());
+  });
 
-    it('als gebruiker wil ik het verschil kunnen zien tussen een error en een gewone knop', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const errorButton = await vlButtonPage.getErrorButton();
-        await assert.eventually.isFalse(primaryButton.isError());
-        await assert.eventually.isTrue(errorButton.isError());
-    });
+  it('als gebruiker wil ik het verschil kunnen zien tussen een error en een gewone knop', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const errorButton = await vlButtonPage.getErrorButton();
+    await assert.eventually.isFalse(primaryButton.isError());
+    await assert.eventually.isTrue(errorButton.isError());
+  });
 
-    it('als gebruiker wil ik het verschil kunnen zien tussen een knop met en zonder icoon', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const iconBeforeButton = await vlButtonPage.getIconBeforeButton();
-        const iconBeforeButtonIcon = await iconBeforeButton.getIcon();
-        await assert.eventually.isFalse(primaryButton.hasIcon());
-        await assert.eventually.isTrue(iconBeforeButton.hasIcon());
-        await assert.eventually.equal(iconBeforeButtonIcon.getType(), 'location');
-    });
+  it('als gebruiker wil ik het verschil kunnen zien tussen een knop met en zonder icoon', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const iconBeforeButton = await vlButtonPage.getIconBeforeButton();
+    const iconBeforeButtonIcon = await iconBeforeButton.getIcon();
+    await assert.eventually.isFalse(primaryButton.hasIcon());
+    await assert.eventually.isTrue(iconBeforeButton.hasIcon());
+    await assert.eventually.equal(iconBeforeButtonIcon.getType(), 'location');
+  });
 
-    it('als gebruiker wil ik een icoon voor en na de tekst in de knop kunnen zien', async () => {
-        const iconBeforeButton = await vlButtonPage.getIconBeforeButton();
-        const iconAfterButton = await vlButtonPage.getIconAfterButton();
-        const iconBeforeButtonIcon = await iconBeforeButton.getIcon();
-        const iconAfterButtonIcon = await iconAfterButton.getIcon();
-        await assert.eventually.equal(iconBeforeButton.getText(), 'Contactpersoon opslaan');
-        await assert.eventually.isTrue(iconBeforeButtonIcon.isBefore());
-        await assert.eventually.isFalse(iconBeforeButtonIcon.isAfter());
-        await assert.eventually.equal(iconAfterButton.getText(), 'Contactpersoon opslaan');
-        await assert.eventually.isFalse(iconAfterButtonIcon.isBefore());
-        await assert.eventually.isTrue(iconAfterButtonIcon.isAfter());
-    });
+  it('als gebruiker wil ik een icoon voor en na de tekst in de knop kunnen zien', async () => {
+    const iconBeforeButton = await vlButtonPage.getIconBeforeButton();
+    const iconAfterButton = await vlButtonPage.getIconAfterButton();
+    const iconBeforeButtonIcon = await iconBeforeButton.getIcon();
+    const iconAfterButtonIcon = await iconAfterButton.getIcon();
+    await assert.eventually.equal(iconBeforeButton.getText(), 'Contactpersoon opslaan');
+    await assert.eventually.isTrue(iconBeforeButtonIcon.isBefore());
+    await assert.eventually.isFalse(iconBeforeButtonIcon.isAfter());
+    await assert.eventually.equal(iconAfterButton.getText(), 'Contactpersoon opslaan');
+    await assert.eventually.isFalse(iconAfterButtonIcon.isBefore());
+    await assert.eventually.isTrue(iconAfterButtonIcon.isAfter());
+  });
 
-    it('als gebruiker wil ik het verschil kunnen zien tussen een block en een gewone knop', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const blockButton = await vlButtonPage.getBlockButton();
-        await assert.eventually.isFalse(primaryButton.isBlock());
-        await assert.eventually.isTrue(blockButton.isBlock());
-    });
+  it('als gebruiker wil ik het verschil kunnen zien tussen een block en een gewone knop', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const blockButton = await vlButtonPage.getBlockButton();
+    await assert.eventually.isFalse(primaryButton.isBlock());
+    await assert.eventually.isTrue(blockButton.isBlock());
+  });
 
-    it('als gebruiker wil ik het verschil kunnen zien tussen een large en een gewone knop', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const largeButton = await vlButtonPage.getLargeButton();
-        await assert.eventually.isFalse(primaryButton.isLarge());
-        await assert.eventually.isTrue(largeButton.isLarge());
-    });
+  it('als gebruiker wil ik het verschil kunnen zien tussen een large en een gewone knop', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const largeButton = await vlButtonPage.getLargeButton();
+    await assert.eventually.isFalse(primaryButton.isLarge());
+    await assert.eventually.isTrue(largeButton.isLarge());
+  });
 
-    it('als gebruiker wil ik het verschil kunnen zien tussen een wide en een gewone knop', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const wideButton = await vlButtonPage.getWideButton();
-        await assert.eventually.isFalse(primaryButton.isWide());
-        await assert.eventually.isTrue(wideButton.isWide());
-    });
+  it('als gebruiker wil ik het verschil kunnen zien tussen een wide en een gewone knop', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const wideButton = await vlButtonPage.getWideButton();
+    await assert.eventually.isFalse(primaryButton.isWide());
+    await assert.eventually.isTrue(wideButton.isWide());
+  });
 
-    it('als gebruiker wil ik het verschil kunnen zien tussen een narrow en een gewone knop', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const narrowButton = await vlButtonPage.getNarrowButton();
-        await assert.eventually.isFalse(primaryButton.isNarrow());
-        await assert.eventually.isTrue(narrowButton.isNarrow());
-    });
+  it('als gebruiker wil ik het verschil kunnen zien tussen een narrow en een gewone knop', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const narrowButton = await vlButtonPage.getNarrowButton();
+    await assert.eventually.isFalse(primaryButton.isNarrow());
+    await assert.eventually.isTrue(narrowButton.isNarrow());
+  });
 
-    it('als gebruiker wil ik het verschil kunnen zien tussen een loading en een gewone knop', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const loadingButton = await vlButtonPage.getLoadingButton();
-        await assert.eventually.isFalse(primaryButton.isLoading());
-        await assert.eventually.isTrue(loadingButton.isLoading());
-    });
+  it('als gebruiker wil ik het verschil kunnen zien tussen een loading en een gewone knop', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const loadingButton = await vlButtonPage.getLoadingButton();
+    await assert.eventually.isFalse(primaryButton.isLoading());
+    await assert.eventually.isTrue(loadingButton.isLoading());
+  });
 
-    it('als gebruiker wil ik het verschil kunnen zien tussen een secondary en een gewone knop', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const secondaryButton = await vlButtonPage.getSecondaryButton();
-        await assert.eventually.isFalse(primaryButton.isSecondary());
-        await assert.eventually.isTrue(secondaryButton.isSecondary());
-    });
+  it('als gebruiker wil ik het verschil kunnen zien tussen een secondary en een gewone knop', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const secondaryButton = await vlButtonPage.getSecondaryButton();
+    await assert.eventually.isFalse(primaryButton.isSecondary());
+    await assert.eventually.isTrue(secondaryButton.isSecondary());
+  });
 
-    it('als gebruiker wil ik het verschil kunnen zien tussen een tertiary en een gewone knop', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const tertiaryButton = await vlButtonPage.getTertiaryButton();
-        await assert.eventually.isFalse(primaryButton.isTertiary());
-        await assert.eventually.isTrue(tertiaryButton.isTertiary());
-    });
+  it('als gebruiker wil ik het verschil kunnen zien tussen een tertiary en een gewone knop', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const tertiaryButton = await vlButtonPage.getTertiaryButton();
+    await assert.eventually.isFalse(primaryButton.isTertiary());
+    await assert.eventually.isTrue(tertiaryButton.isTertiary());
+  });
 
-    it('als gebruiker wil ik een knop kunnen zien met alleen maar een icoon', async () => {
-        const primaryButton = await vlButtonPage.getPrimaryButton();
-        const iconButton = await vlButtonPage.getIconButton();
-        await assert.eventually.equal(primaryButton.hasText(), true);
-        await assert.eventually.equal(primaryButton.hasIcon(), false);
-        await assert.eventually.equal(iconButton.hasText(), false);
-        await assert.eventually.equal(iconButton.hasIcon(), true);
-    });
+  it('als gebruiker wil ik een knop kunnen zien met alleen maar een icoon', async () => {
+    const primaryButton = await vlButtonPage.getPrimaryButton();
+    const iconButton = await vlButtonPage.getIconButton();
+    await assert.eventually.equal(primaryButton.hasText(), true);
+    await assert.eventually.equal(primaryButton.hasIcon(), false);
+    await assert.eventually.equal(iconButton.hasText(), false);
+    await assert.eventually.equal(iconButton.hasIcon(), true);
+  });
 
-    it('als gebruiker zie ik een link button', async () => {
-        const linkButton = await vlButtonPage.getLinkButton();
-        await assert.eventually.isTrue(linkButton.isDisplayed());
-        await assert.eventually.equal(linkButton.getText(), 'Ga naar startpagina');
-    });
+  it('als gebruiker zie ik een link button', async () => {
+    const linkButton = await vlButtonPage.getLinkButton();
+    await assert.eventually.isTrue(linkButton.isDisplayed());
+    await assert.eventually.equal(linkButton.getText(), 'Ga naar startpagina');
+  });
 });
--- test/e2e/components/vl-button.js
+++ test/e2e/components/vl-button.js
@@ -1,65 +1,63 @@
-const { VlElement } = require('vl-ui-core').Test;
-const { By } = require('selenium-webdriver');
+const {VlElement} = require('vl-ui-core').Test;
+const {By} = require('selenium-webdriver');
 
-class VlButtonElement extends VlElement {  
-
-    async getIcon() {
-        const icon = await this.findElement(By.css('[is="vl-icon"]'));
-        if (icon) {
-            const { VlIcon } = require('vl-ui-icon').Test;
-            return new VlIcon(this.driver, icon);
-        }
+class VlButtonElement extends VlElement {
+  async getIcon() {
+    const icon = await this.findElement(By.css('[is="vl-icon"]'));
+    if (icon) {
+      const {VlIcon} = require('vl-ui-icon').Test;
+      return new VlIcon(this.driver, icon);
     }
+  }
 
-    async isError() {
-        return this.hasAttribute('error');
-    }
+  async isError() {
+    return this.hasAttribute('error');
+  }
 
-    async isBlock() {
-        return this.hasAttribute('block');
-    }
+  async isBlock() {
+    return this.hasAttribute('block');
+  }
 
-    async isLarge() {
-        return this.hasAttribute('large');
-    }
+  async isLarge() {
+    return this.hasAttribute('large');
+  }
 
-    async isWide() {
-        return this.hasAttribute('wide');
-    }
+  async isWide() {
+    return this.hasAttribute('wide');
+  }
 
-    async isNarrow() {
-        return this.hasAttribute('narrow');
-    }
+  async isNarrow() {
+    return this.hasAttribute('narrow');
+  }
 
-    async isLoading() {
-        return this.hasAttribute('loading');
-    }
+  async isLoading() {
+    return this.hasAttribute('loading');
+  }
 
-    async isSecondary() {
-        return this.hasAttribute('secondary');
-    }
+  async isSecondary() {
+    return this.hasAttribute('secondary');
+  }
 
-    async isTertiary() {
-        return this.hasAttribute('tertiary');
-    }
+  async isTertiary() {
+    return this.hasAttribute('tertiary');
+  }
 
-    async hasIcon() {
-        try {
-            await this.getIcon();
-            return true;
-        } catch(e) {
-            return false;
-        }
+  async hasIcon() {
+    try {
+      await this.getIcon();
+      return true;
+    } catch (e) {
+      return false;
     }
+  }
 }
 
-class VlButton extends VlButtonElement  { }
+class VlButton extends VlButtonElement { }
 
-class VlLinkButton extends VlButtonElement { } 
+class VlLinkButton extends VlButtonElement { }
 
 
-
-module.exports = { 
-    VlButton, 
-    VlLinkButton
+module.exports = {
+  VlButton,
+  VlLinkButton,
 };
