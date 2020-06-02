1a0f8d9f5c8ca5a68080c7064d0561f25ce98e7d
Tom Coemans <tom.coemans@gmail.com>
2020-06-02 15:51:14 +0200

    ESlint

--- src/vl-button.js
+++ src/vl-button.js
@@ -1,51 +1,54 @@
-import { NativeVlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.js';
+import {nativeVlElement, define} from '/node_modules/vl-ui-core/dist/vl-core.js';
 
-export const VlButtonElement = (SuperClass) => {
-    return class extends NativeVlElement(SuperClass) {
-        static get _observedAttributes() {
-            return [];
-        }
-
-        static get _observedClassAttributes() {
-            return ['disabled', 'error', 'block', 'large', 'wide', 'narrow', 'secondary', 'tertiary', 'loading'];
-        }
-
-        connectedCallback() {
-            this.classList.add('vl-button');
-            setTimeout(() => {
-                this._setIconClass();
-            });
-        }
-
-        get _classPrefix() {
-            return 'vl-button--';
-        }
-
-        _setIconClass() {
-            const icon = this.querySelector('[is="vl-icon"]');
-            if (icon) {
-                let suffix = '';
-                suffix += icon.hasAttribute('data-vl-before') || icon.hasAttribute('before') ? '-before' : '';
-                suffix += icon.hasAttribute('data-vl-after') || icon.hasAttribute('after') ? '-after' : '';
-                this.classList.add(this._classPrefix + 'icon' + suffix);
-                icon.classList.add('vl-button__icon');
-                if (suffix) {
-                    icon.classList.add('vl-button__icon-' + suffix);
-                }
-                icon.classList.remove('vl-icon--before');
-                icon.classList.remove('vl-icon--after');
-            }
-        }
+export const vlButtonElement = (SuperClass) => {
+  return class extends nativeVlElement(SuperClass) {
+    static get _observedAttributes() {
+      return [];
     }
+
+    static get _observedClassAttributes() {
+      return ['disabled', 'error', 'block', 'large', 'wide', 'narrow', 'secondary', 'tertiary', 'loading'];
+    }
+
+    connectedCallback() {
+      this.classList.add('vl-button');
+      setTimeout(() => {
+        this._setIconClass();
+      });
+    }
+
+    get _classPrefix() {
+      return 'vl-button--';
+    }
+
+    _setIconClass() {
+      const icon = this.querySelector('[is="vl-icon"]');
+      if (icon) {
+        let suffix = '';
+        suffix += icon.hasAttribute('data-vl-before') || icon.hasAttribute('before') ? '-before' : '';
+        suffix += icon.hasAttribute('data-vl-after') || icon.hasAttribute('after') ? '-after' : '';
+        this.classList.add(this._classPrefix + 'icon' + suffix);
+        icon.classList.add('vl-button__icon');
+        if (suffix) {
+          icon.classList.add('vl-button__icon-' + suffix);
+        }
+        icon.classList.remove('vl-icon--before');
+        icon.classList.remove('vl-icon--after');
+      }
+    }
+  };
 };
+
+export const VlButtonElement = vlButtonElement;
 
 /**
  * VlButton
  * @class
- * @classdesc Gebruik de vl-button om een ​​call-to-action toe te voegen. Het type call-to-action wordt bepaald door het label of de pictogram.
- * 
- * @extends NativeVlElement
- * 
+ * @classdesc Gebruik de vl-button om een CTA toe te voegen. Het type call-to-action wordt bepaald door het label of de pictogram.
+ *
+ * @extends HTMLButtonElement
+ * @mixin nativeVlElement
+ *
  * @property {boolean} disabled - Attribuut wordt gebruikt om aan de gebruiker aan te duiden dat de functionaliteit niet actief is.
  * @property {boolean} error - Attribuut wordt gebruikt om het belang of de gevolgen van een actie te benadrukken.
  * @property {boolean} block - Attribuut wordt gebruikt om ervoor te zorgen dat de knop getoond wordt als een block element en bijgevol de breedte van de parent zal aannemen.
@@ -55,17 +58,17 @@ export const VlButtonElement = (SuperClass) => {
  * @property {boolean} loading - Attribuut wordt gebruikt om aan de gebruiker aan te geven dat zijn actie momenteel verwerkt wordt.
  * @property {boolean} secondary - Attribuut wordt gebruikt in combinatie met een gewone knop om alternatieve acties te voorzien.
  * @property {boolean} tertiary - Attribuut wordt gebruikt in combinatie met gewone en secondary knoppen om alternatieve acties te voorzien.
- * 
+ *
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-button/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-button/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-button.html|Demo}
  */
-export class VlButton extends VlButtonElement(HTMLButtonElement) { }
+export class VlButton extends vlButtonElement(HTMLButtonElement) { }
 
 /**
  * VlLinkButton
  * @class
- * @classdesc Gebruik de vl-link-button om een ​​call-to-action toe te voegen.
+ * @classdesc Gebruik de vl-link-button om een CTA toe te voegen.
  *
  * @extends NativeVlElement
  *
@@ -73,7 +76,7 @@ export class VlButton extends VlButtonElement(HTMLButtonElement) { }
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-link/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-link.html|Demo}
  */
-export class VlLinkButton extends VlButtonElement(HTMLAnchorElement) { }
+export class VlLinkButton extends vlButtonElement(HTMLAnchorElement) { }
 
 define('vl-button', VlButton, {extends: 'button'});
 define('vl-link-button', VlLinkButton, {extends: 'a'});
